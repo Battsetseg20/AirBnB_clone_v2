19037ae9d60569889169670d7a7da45032357a88
Battsetseg20 <tsetseg.yo@gmail.com>
2021-04-15 11:26:21 +0000

    task 0: set up web server for deployment of web static

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+# Sets up the web server for the deployment of web_static
+
+# Install Nginx if not already installed
+sudo apt-get update
+sudo apt-get install -y nginx
+
+#Create the folder /data/web_static/releases/ if it doesn’t already exist
+sudo mkdir -p /data/web_static/releases/
+
+#Create the folder /data/web_static/shared/ if it doesn’t already exist
+sudo mkdir -p /data/web_static/shared/
+
+#Create the folder /data/web_static/releases/test/ if it doesn’t already exist
+sudo mkdir /data/web_static/releases/test
+
+#Create a fake HTML file /data/web_static/releases/test/index.html w/ simple content
+echo "Holberton School" | sudo tee  /data/web_static/releases/test/index.html
+
+#Create a symbolic link /data/web_static/current linked to the /data/web_static/releases/test/ folder.
+#If the symbolic link already exists, it should be deleted and recreated every time the script is ran.
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+
+#Give recursive ownership of the /data/ folder to the ubuntu user AND group
+sudo chown -hR ubuntu:ubuntu /data/
+
+#Update the Nginx configuration to serve the content of /data/web_static/current/ to hbnb_static
+sudo sed -i '7i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+
+
+sudo service nginx start
